847009573c237b0074cb0ed42f8458d0be2fb9e8
Rob Sioss <39536918+rsioss@users.noreply.github.com>
2023-06-27 21:20:01 -0400

    feat: add support for optional mapping in transaction-to-webhook

--- src/functions/transaction-to-webhook/handler.ts
+++ src/functions/transaction-to-webhook/handler.ts
@@ -1,8 +1,14 @@
 import { type CoreTransactionProcessedEvent } from "@stedi/integrations-sdk";
-import { bucketsClient } from "@stedi/integrations-sdk/clients";
+import { bucketsClient, mappingsClient } from "@stedi/integrations-sdk/clients";
 import { GetObjectCommand } from "@stedi/sdk-client-buckets";
+import { MapDocumentCommand } from "@stedi/sdk-client-mappings";
 
 export const handler = async (event: CoreTransactionProcessedEvent) => {
+  // fail fast if WEBHOOK_URL env var is not defined
+  if (process.env.WEBHOOK_URL === undefined) {
+    throw new Error("WEBHOOK_URL is not defined");
+  }
+
   // get bucket reference for the JSON version of EDI Transaction Set
   const {
     detail: { output },
@@ -20,9 +26,10 @@ export const handler = async (event: CoreTransactionProcessedEvent) => {
 
   const body = await getFile.body.transformToString();
 
-  if (process.env.WEBHOOK_URL === undefined) {
-    throw new Error("WEBHOOK_URL is not defined");
-  }
+  const webhookPayload =
+    process.env.MAPPING_ID === undefined
+      ? body
+      : await invokeMapping(process.env.MAPPING_ID, body);
 
   // send JSON to endpoint
   const result = await fetch(process.env.WEBHOOK_URL, {
@@ -33,8 +40,28 @@ export const handler = async (event: CoreTransactionProcessedEvent) => {
         Authorization: process.env.AUTHORIZATION,
       }),
     },
-    body,
+    body: webhookPayload,
   });
 
   return { ok: result.ok, statusCode: result.status };
+};
+
+const invokeMapping = async (
+  mappingId: string,
+  input: string
+): Promise<string> => {
+  const mappingResult = await mappingsClient().send(
+    new MapDocumentCommand({
+      id: mappingId,
+      content: input,
+    })
+  );
+
+  if (!mappingResult.content) {
+    throw new Error(
+      `map (id=${mappingId}) operation did not return any content`
+    );
+  }
+
+  return JSON.stringify(mappingResult.content);
 };
